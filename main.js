ff9bf17b186708955c1d44fa213483208968e8c1
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-05 13:53:04 +0400

    c

---
+++ main.js
@@ -0,0 +1,15 @@
+import { Telegraf, Markup } from "telegraf"
+
+const token = "6801500269:AAHiSVv0nRxY-cGunncIGP1Sh_RAfmK_p90"
+const webAppUrl = "https://edwardkirakosyan.com/"
+
+const bot = new Telegraf(token)
+
+bot.command("start", (ctx) => {
+  ctx.reply(
+    "Welcome! Click button below to start APP",
+    Markup.keyboard([Markup.button.webApp("Send message", webAppUrl)])
+  )
+})
+
+bot.launch()
